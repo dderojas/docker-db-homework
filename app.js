0fbfe52809fc8671a74c52a628b6eebfee352bab
Don de Rojas <derojasdonny@gmail.com>
2022-11-22 21:23:55 -0800

    add gitignore, add code comments

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const fs = require('fs')
-const { Transform, pipeline } = require('stream')
+const { Transform } = require('stream')
 const { Pool } = require('pg')
 const QueryStream = require('pg-query-stream')
 const { stringify } = require('csv')
+const isEqual = require('lodash.isequal')
 
 const poolOld = new Pool({
   host: 'localhost',
@@ -17,20 +18,75 @@ const poolNew = new Pool({
   user: 'new',
   password: 'hahaha',
 });
-  
-  
 
-(async () => {
-  try {
-    await poolOld.connect() 
-    console.log('oldDB connected!!!') 
+
+
+const newDBColumns = [
+  "id",
+  "name",
+  "email",
+  "favorite_flavor",
+  "not_in_new_db",
+  "corrupted",
+  "newRecord"
+];
+
+const oldDBColumns = [
+  "id",
+  "name",
+  "email",
+  "favorite_flavor",
+  "not_in_new_db",
+  "corrupted",
+  "newRecord"
+];
+
+const migratedDBreport = fs.createWriteStream('./migratedDBreport.csv')
+const oldDBreport = fs.createWriteStream('./oldDBreport.csv')
+
+const stringifyMigratedData = stringify({ header: true, columns: newDBColumns });
+const stringifyOldData = stringify({ header: true, columns: oldDBColumns });
+
+
+try {
+  poolOld.connect((err, client, done) => {
+      console.log('oldDB connected!!!') 
+
+      if (err) throw err
+      // create stream from old DB
+      const query = new QueryStream(`select * from accounts limit 5`)
+      const stream = client.query(query)
+
+      const isInTable = new Transform({
+        objectMode: true,
+        async transform(chunk, encoding, callback) {
+          stream.pause()
+          // make sure records from old DB are in the new DB
+          let results = await poolNew.query(`SELECT * FROM accounts WHERE id = $1`, [chunk['id']])
+          stream.resume()
+
+          // save records that didn't make it to new DB
+          if (results.rows.length === 0) {
+            chunk['not_in_new_db'] = 'true'
+            stringifyOldData.write(chunk)
+          }
+          callback(null)
+        },
+      });
+
+      stream.on('end', async () => {
+        done()
+        console.log('FINISHED oldDB!!!!!!!')
+      })
+      
+      stream.pipe(isInTable).pipe(stringifyOldData).pipe(oldDBreport)
+
+    })
   } catch(e) {
-    console.error(e, 'error in first client')
+      console.error(e)
   }
-})();
 
 
-(() => {
   
   try {
     poolNew.connect((err, client, done) => {
@@ -38,57 +94,45 @@ const poolNew = new Pool({
 
       
       if (err) throw err
-      const query = new QueryStream(`select * from accounts limit 10`)
+      // create stream from new DB
+      const query = new QueryStream(`select * from accounts limit 5`)
       const stream = client.query(query)
-      const writeStuff = fs.createWriteStream('./something.csv')
-      /*
-      {
-        id,
-        name,
-        email,
-        favorite_flavor,
-        missingInNewDB: boolean
-        corrupted: boolean
-        newRecord: boolean
-      }
-      */
-      const columns = [
-        "id",
-        "name",
-        "email",
-        "favorite_flavor",
-        "not_in_new_db",
-        "corrupted",
-        "newRecord"
-      ];
-      const stringifier = stringify({ header: true, columns: columns });
 
       const isInTable = new Transform({
         objectMode: true,
         async transform(chunk, encoding, callback) {
-          // console.log(chunk, 'CHUNK!@!@#')
           stream.pause()
+
+          // check to see if data from new DB is from the old DB
           let results = await poolOld.query(`SELECT * FROM accounts WHERE id = $1`, [chunk['id']])
-          // console.log(results.rows, 'after results!@#')
           stream.resume()
 
+          // record new records
           if (results.rows.length === 0) {
-            console.log(chunk, 'chunkinIFFFF')
+
             chunk['newRecord'] = 'true'
-            stringifier.write(chunk)
-            callback(null);
-          } else {
-            callback(null)
+            stringifyMigratedData.write(chunk)
+
+          // check that migrated records were not corrupted  
+          } else if (!isEqual(chunk, results.rows[0])) {
+
+            chunk['corrupted'] = 'true'
+            stringifyMigratedData.write(chunk)
+
           }
+          callback(null)
         },
       });
 
-      stream.on('end', done)
+      stream.on('end', async () => {
+        done()
+        console.log('FINISHED!!!!!!!')
+      })
       
-      stream.pipe(isInTable).pipe(stringifier).pipe(writeStuff)
+      stream.pipe(isInTable).pipe(stringifyMigratedData).pipe(migratedDBreport)
     })
   } catch(e) {
     console.error(e, 'error in first client')
   }
-})();
+
 
